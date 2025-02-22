392f515e05384eed14e62952ef41d62afed2e72b
malavikasmenon <malavikamenon2000@gmail.com>
2025-02-22 18:22:18 +0530

    call parse methods one after the other

--- scraping/scraping/spiders/goodreads_profile.py
+++ scraping/scraping/spiders/goodreads_profile.py
@@ -8,57 +8,96 @@ class GoodreadsProfileSpider(scrapy.Spider):
 
     def parse(self, response):
         """
-        Scrape three main categories
-        1. Want to read
-        2. Currently Reading
+        Scrape three main categories sequentially:
+        1. Currently Reading
+        2. Want to Read
         3. Read
         """
         profile_name = response.xpath('//*[@id="profileNameTopHeading"]/text()').get()
-        shelves = ["currently-reading", "to-read", "read"]
         user_data = {
             "profile_name": profile_name.strip() if profile_name else None,
             "currently-reading": [],
-            "to-read": [],
-            "read": []
+            "read": [],
+            "to-read": []
         }
         
-        for shelf in shelves:
-            shelf_xpath = f"//a[contains(@href, 'shelf={shelf}')]/@href"
-            print(shelf_xpath)
-            link = response.xpath(shelf_xpath).get()
-            print(link)
-            full_url = response.urljoin(link)  # Convert relative to absolute URL
-            print(full_url)
-            yield scrapy.Request(full_url, callback=self.parse_shelf, meta={"user_data": user_data, "shelf": shelf})
-
-
-        # yield {"name": profile_name}
-
-    def parse_shelf(self, response):
+        yield from self.parse_current_reads(response, user_data)
+    
+    def parse_current_reads(self, response, user_data):
+        shelf = "currently-reading"
+        shelf_xpath = f"//a[contains(@href, 'shelf={shelf}')]/@href"
+        link = response.xpath(shelf_xpath).get()
+        
+        if link:
+            full_url = response.urljoin(link)
+            yield scrapy.Request(full_url, callback=self.parse_books, meta={
+                "user_data": user_data,
+                "next_callback": self.parse_to_read,
+                "shelf": "currently-reading"
+            })
+        else:
+            yield from self.parse_to_read(response, user_data)
+    
+    def parse_to_read(self, response, user_data):
+        shelf = "to-read"
+        shelf_xpath = f"//a[contains(@href, 'shelf={shelf}')]/@href"
+        link = response.xpath(shelf_xpath).get()
+        
+        if link:
+            full_url = response.urljoin(link)
+            yield scrapy.Request(full_url, callback=self.parse_books, meta={
+                "user_data": user_data,
+                "next_callback": self.parse_read,
+                "shelf": "to-read"
+            })
+        else:
+            yield from self.parse_read(response, user_data)
+    
+    def parse_read(self, response, user_data):
+        shelf = "read"
+        shelf_xpath = f"//a[contains(@href, 'shelf={shelf}')]/@href"
+        link = response.xpath(shelf_xpath).get()
+        
+        if link:
+            full_url = response.urljoin(link)
+            yield scrapy.Request(full_url, callback=self.parse_books, meta={
+                "user_data": user_data,
+                "next_callback": None,
+                "shelf": "read"
+            })
+        else:
+            yield user_data
+    
+    def parse_books(self, response):
         """
-        Book Title
-        Book Author
-        Book Rating
+        Extract book details from a given shelf.
         """
         book_rows = response.xpath("//tbody/tr[contains(@class, 'bookalike')]")
         user_data = response.meta["user_data"]
+        next_callback = response.meta["next_callback"]
         shelf = response.meta["shelf"]
-        
+
         for row in book_rows:
             book_title = row.xpath(".//td[contains(@class, 'title')]/div/a/@title").get()
             book_isbn = row.xpath(".//td[contains(@class, 'isbn')]/div/text()").get()
             book_author = row.xpath(".//td[contains(@class, 'author')]/div/a/text()").get()
+            
             book_logged = {
                 "title": book_title.strip() if book_title else None,
                 "isbn": book_isbn.strip() if book_isbn else None,
                 "author": book_author.strip() if book_author else None,
+                # "shelf": response.url.split("shelf=")[-1]
             }
             user_data[shelf].append(book_logged)
 
         next_page = response.xpath("//a[contains(@class, 'next_page')]/@href").get()
-    
         if next_page:
-            yield response.follow(next_page, callback=self.parse_shelf, meta={"user_data": user_data, "shelf": shelf})
-        
-        if all(user_data[s] for s in ["currently-reading", "to-read", "read"]):
+            yield response.follow(next_page, callback=self.parse_books, meta={
+                "user_data": user_data,
+                "next_callback": next_callback,
+                "shelf": shelf
+            })
+        elif next_callback:
+            yield from next_callback(response, user_data)
+        else:
             yield user_data
